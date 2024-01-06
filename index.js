8fc7bcb6cfbaaa11744fc7e1870a6ffca7d42c5b
Joao Carlos <jocafs08@gmail.com>
2024-01-06 12:32:43 -0300

    atualizadas mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import {turboLogger} from './turboLogger.js'
 
 console.log('Estou usando o github')
 turboLogger('estou aprendendo o fluso do git');
+turboLogger('nova mensagem sem afetar a principal', 'darkgreen')
